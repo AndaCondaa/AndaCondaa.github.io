911a932d922ea06921037e3f7e5b8caac6790988
AndaCondaa <simon5229@naver.com>
2024-05-23 02:37:49 +0900

    jquery

--- colors.js
+++ colors.js
@@ -1,20 +1,24 @@
 var Link = {
     setColor: function (color) {
-        var alist = document.querySelectorAll('a');
-        var i = 0;
-        while (i < alist.length) {
-            if (alist[i].id != document.getElementById('title_main').id)
-                alist[i].style.color = color;
-            i = i + 1;
-        }
+        // var alist = document.querySelectorAll('a');
+        // var i = 0;
+        // while (i < alist.length) {
+        //     if (alist[i].id != document.getElementById('title_main').id)
+        //         alist[i].style.color = color;
+        //     i = i + 1;
+        // }
+        if ($('a').id != 'title_main')
+            $('a').not('#title_main').css('color', color);
     }
 }
 var Body = {
     setColor: function (color) {
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor: function (color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor',color);
 
     }
 }
