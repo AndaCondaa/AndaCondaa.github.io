f308214d06431dda56fe54c0af530de8e7aa085d
AndaCondaa <simon5229@naver.com>
2024-05-26 23:41:24 +0900

    vue

---
+++ Vue/vuedongsan/src/App.vue
@@ -0,0 +1,29 @@
+// HTML
+<template> 
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+// JS
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+// CSS
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
