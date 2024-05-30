84b98b12857126208505feeef32f9d4888bfee86
AndaCondaa <simon5229@naver.com>
2024-05-30 23:57:59 +0900

    vue

--- Vue/vuedongsan/src/App.vue
+++ Vue/vuedongsan/src/App.vue
@@ -4,38 +4,53 @@
     <a v-for="el in menu_name" :key="el">{{ el }}</a>
   </div>
 
-  <div v-for="el in products" :key="el">
-    <div class="black-bg" v-if="el.state == true">
-      <div class="white-bg">
-        <h4>{{ el.name }}</h4>
-        <p>{{el.name}}의 가격은 {{el.price}} 만원입니다.<br>{{el.content}} <br>허위매물신고 건수는 {{el.report}}입니다.</p>
-        <button @click="el.state = false">닫기</button>
-      </div>
-    </div>
-  </div>
+  <Discount />
 
-  <div v-for="(el,i) in products" :key="el">
-    <img  v-bind:src="el.image" class="room-img" />
-    <h4 @click="el.state = true" :style="product_text_color">
-      {{el.title }}
+  <div v-for="(el, i) in products" :key="el">
+    <img v-bind:src="el.image" class="room-img" />
+    <h4
+      @click="
+        this.detail_state = true;
+        this.detail_page = i;
+      "
+      :style="product_text_color"
+    >
+      {{ el.title }}
     </h4>
     <p>{{ el.price }} 만원</p>
     <button @click="increase(i)" class="button-report">허위매물신고</button>
     <span>신고수 :{{ el.report }}</span>
   </div>
+
+  <div class="black-bg" v-if="detail_state == true">
+    <div class="white-bg">
+      <h4 style="display: inline">{{ products[detail_page].title }}</h4>
+      <button
+        style="display: inline; position: absolute; right: 20px"
+        @click="this.detail_state = false"
+      >
+        닫기
+      </button>
+      <p>
+        {{ products[detail_page].title }}의 가격은
+        {{ products[detail_page].price }} 만원입니다.<br />
+        {{ products[detail_page].content }} 허위매물신고 건수는
+        {{ products[detail_page].report }}입니다.
+      </p>
+    </div>
+  </div>
 </template>
 
 // JS
 <script>
-
-import data from './assets/data.js'
-
-
+import data from "./assets/data.js";
 
 export default {
   name: "App",
   data() {
     return {
+      detail_page: 0,
+      detail_state: false,
       menu_name: ["Home", "Shop", "About"],
       products: data,
       product_text_color: "color : blue",
--- Vue/vuedongsan/src/assets/data.js
+++ Vue/vuedongsan/src/assets/data.js
@@ -4,7 +4,6 @@ export default [{
     image: "https://codingapple1.github.io/vue/room0.jpg",
     content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
     price: 340000,
-    state: false,
     report: 0
 },
 {
@@ -13,7 +12,6 @@ export default [{
     image: "https://codingapple1.github.io/vue/room1.jpg",
     content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
     price: 450000,
-    state: false,
     report: 0
 },
 {
@@ -22,7 +20,6 @@ export default [{
     image: "https://codingapple1.github.io/vue/room2.jpg",
     content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
     price: 780000,
-    state: false,
     report: 0
 },
 {
@@ -31,7 +28,6 @@ export default [{
     image: "https://codingapple1.github.io/vue/room3.jpg",
     content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
     price: 550000,
-    state: false,
     report: 0
 },
 {
@@ -40,7 +36,6 @@ export default [{
     image: "https://codingapple1.github.io/vue/room4.jpg",
     content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
     price: 680000,
-    state: false,
     report: 0
 },
 {
@@ -49,7 +44,6 @@ export default [{
     image: "https://codingapple1.github.io/vue/room5.jpg",
     content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
     price: 370000,
-    state: false,
     report: 0
 }];
 
