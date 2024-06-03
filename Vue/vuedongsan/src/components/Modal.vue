cbf2e207efc30f7b55e2c7a08f92fac962edad06
AndaCondaa <simon5229@naver.com>
2024-06-04 00:11:44 +0900

    0604

--- Vue/vuedongsan/src/components/Modal.vue
+++ Vue/vuedongsan/src/components/Modal.vue
@@ -7,32 +7,33 @@
         @click="$emit('closeModal')"
       >
         닫기
-      </button>      
+      </button>
       <p>
         {{ products[detail_page].title }}의 가격은
         {{ products[detail_page].price }} 만원입니다.<br />
         {{ products[detail_page].content }} 허위매물신고 건수는
         {{ products[detail_page].report }}입니다.
       </p>
-      <input @input="month = $event.target.value">
-      <p>{{ products[detail_page].price  * month}} 만원</p>
+      <!-- <input @input="month = $event.target.value" /> -->
+      <input v-model.number="month" />
+      <p>{{ month }}개월 : {{ products[detail_page].price * month }} 만원</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-    name : "Modal",
-    data() {
-      return {
-        month : 1,
-      }
-    },
-    props : {
-        products : Array,
-        detail_page : Number,
-        detail_state : Boolean,
-    }
+  name: "Modal",
+  data() {
+    return {
+      month: 12,
+    };
+  },
+  props: {
+    products: Array,
+    detail_page: Number,
+    detail_state: Boolean,
+  },
 };
 </script>
 
